8382534fc348384e5e7467bcc98c54693ff70529
root <root@instance-1.us-central1-a.c.phoenixforce-170809.internal>
2020-03-05 20:39:04 +0000

    added if-else condition to mkdockerize.sh so it either serves or produces depending upon user args  for path  for image tag and  for serve or prouce.

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -1,5 +1,13 @@
-#Producing the website
-docker run --rm --name mkdocs-produce -v $1:/docs anilmahapatra/mkdocs:$2 produce
+#!/bin/bash
 
-#Running the website
-docker run -d --rm -p 8000:8000 --name mkdocs-serve -v $1:/docs anilmahapatra/mkdocs:$2 serve
+if [ "$3" = "serve" ]
+then
+   #Running the website
+    docker run -d --rm -p 8000:8000 --name mkdocs-serve -v $1:/docs anilmahapatra/mkdocs:$2 $3
+elif [ "$3" = "produce" ]
+then
+    #Producing the website
+    docker run --rm --name mkdocs-produce -v $1:/docs anilmahapatra/mkdocs:$2 $3
+else
+    echo "This argument is invalid. Please provide [serve or produce]"
+fi
